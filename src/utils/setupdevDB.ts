883332eea77951610404f10c85b91055aaaeba6c
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-03-23 19:49:51 -0500

    added first data boiletplate

--- src/utils/setupdevDB.ts
+++ src/utils/setupdevDB.ts
@@ -1,7 +1,101 @@
 import { getDBConfig } from '@config/database';
+import { Category } from '@entities/Category.entity';
+import { Question } from '@entities/Question.entity';
+import { QuestionItems } from '@entities/QuestionItems.entity';
+import { Section } from '@entities/Section.entity';
+import { SubSection } from '@entities/SubSection.entity';
+import { getConnection } from 'typeorm';
 
 export const connectSqlDB = async () => await getDBConfig();
 
 export const boilerplateData = async () => {
-  
+  let openQuestionCategory = await Category.findOne({ name: 'Open' });
+  if (!openQuestionCategory) {
+    openQuestionCategory = await Category.create({ name: 'Open' }).save();
+  }
+
+  let comboQuestionCategory = await Category.findOne({ name: 'Combo' });
+  if (!comboQuestionCategory) {
+    comboQuestionCategory = await Category.create({ name: 'Combo' }).save();
+  }
+
+  let LakeNonaSection = await Section.findOne({ name: 'Lake-Nona' });
+  if (!LakeNonaSection) {
+    LakeNonaSection = await Section.create({ name: 'Lake-Nona', order: 0 }).save();
+  }
+
+  let LakeNonaSubSection = await SubSection.findOne({ name: 'Lake-Nona' });
+  if (!LakeNonaSubSection) {
+    LakeNonaSubSection = await SubSection.create({ name: 'Lake-Nona', order: 0 }).save();
+  }
+  await getConnection()
+    .createQueryBuilder()
+    .relation(SubSection, 'section')
+    .of(LakeNonaSubSection)
+    .set(LakeNonaSection);
+
+  let LakeNonaFirstQuestion = await Question.findOne({ question: 'Do you live in lake nona?' });
+  if (!LakeNonaFirstQuestion) {
+    LakeNonaFirstQuestion = await Question.create({
+      question: 'Do you live in lake nona?',
+
+      stack: 0,
+
+      stackPhrase: 'Whatever phrase',
+
+      placeHolder: 'Yes/no',
+
+      order: 0,
+
+      inputConfirmation: 'Alpha',
+    }).save();
+  }
+
+  await getConnection()
+    .createQueryBuilder()
+    .relation(Question, 'category')
+    .of(LakeNonaFirstQuestion)
+    .set(openQuestionCategory);
+
+  await getConnection()
+    .createQueryBuilder()
+    .relation(Question, 'subSection')
+    .of(LakeNonaFirstQuestion)
+    .set(LakeNonaSubSection);
+
+  let LakeNonaSecondQuestion = await Question.findOne({ question: 'Select one of this?' });
+  if (!LakeNonaSecondQuestion) {
+    LakeNonaSecondQuestion = await Question.create({
+      question: 'Select one of this?',
+
+      stack: 0,
+
+      stackPhrase: 'Whatever phrase',
+
+      placeHolder: 'Yes/no',
+
+      order: 1,
+
+      inputConfirmation: 'Alpha',
+    }).save();
+  }
+
+  let questionItem1 = await QuestionItems.findOne({ name: 'this is an option' });
+  if (!questionItem1) {
+    questionItem1 = await QuestionItems.create({ name: 'this is an option', order: 0 }).save();
+  }
+
+  await getConnection()
+    .createQueryBuilder()
+    .relation(Question, 'category')
+    .of(LakeNonaSecondQuestion)
+    .set(comboQuestionCategory);
+
+  await getConnection()
+    .createQueryBuilder()
+    .relation(Question, 'subSection')
+    .of(LakeNonaSecondQuestion)
+    .set(LakeNonaSubSection);
+
+  await getConnection().createQueryBuilder().relation(Question, 'items').of(LakeNonaSecondQuestion).add(questionItem1);
 };
